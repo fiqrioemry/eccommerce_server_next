e71a3af44b835b9c5dd9cde5b303ea57e77c0c83
fiqrioemry <foemry@gmail.com>
2024-11-02 23:43:15 +0700

    setting environment

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  development: {
+    username: "root",
+    password: "Oemry241995@",
+    database: "ecommerce_server",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};

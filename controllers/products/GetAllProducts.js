f893370fc93007f167577cebc6368fcb57c60901
fiqrioemry <foemry@gmail.com>
2024-11-15 22:07:27 +0700

    fix error and wrong syntax

--- controllers/products/GetAllProducts.js
+++ controllers/products/GetAllProducts.js
@@ -11,18 +11,18 @@ const {
 module.exports = async (req, res) => {
   try {
     const {
-      limit,
-      page,
       search,
       category,
       minPrice,
       maxPrice,
       minScore,
       maxScore,
+      limit,
+      page,
     } = req.query;
 
-    const dataPerPage = parseInt(limit) || 10;
-    const currentPage = parseInt(page) || 0;
+    const dataPerPage = parseInt(limit) || 8;
+    const currentPage = parseInt(page) || 1;
     const offset = currentPage - 1 * dataPerPage;
 
     const query = {};
@@ -33,7 +33,48 @@ module.exports = async (req, res) => {
       ];
     }
 
-    const product = await Products.findAll({
+    if (category) {
+      const found = await Categories.findOne({
+        where: { slug: category },
+      });
+      if (found) {
+        query.categoryId = found.id;
+      } else {
+        return res.status(404).send({ message: "Category not found" });
+      }
+    }
+    if (minPrice & maxPrice) {
+      query.price = {
+        [Op.between]: [minPrice, maxPrice],
+      };
+    } else if (minPrice) {
+      query.price = {
+        [Op.gte]: minPrice,
+      };
+    } else if (maxPrice) {
+      query.price = {
+        [Op.lte]: maxPrice,
+      };
+    }
+
+    if (minScore & maxScore) {
+      query.rating = {
+        [Op.between]: [minScore, maxScore],
+      };
+    } else if (minScore) {
+      query.rating = {
+        [Op.gte]: [minScore],
+      };
+    } else if (maxScore) {
+      query.rating = {
+        [Op.lte]: [maxScore],
+      };
+    }
+
+    const product = await Products.findAndCountAll({
+      where: query,
+      limit: dataPerPage,
+      offset: offset,
       include: [
         {
           model: Stores,
@@ -43,34 +84,52 @@ module.exports = async (req, res) => {
         { model: OrderDetails },
         { model: Reviews },
       ],
+      disctinct: true,
+      order: [["createdAt", "DESC"]],
     });
 
-    const data = product.map((item) => {
-      return {
-        id: item.id,
-        title: item.name,
-        category: item.Category.name,
-        description: item.description,
-        price: item.price,
-        stock: item.stock,
-        images: item.Images.map((item) => item.image),
-        sold: item?.OrderDetails.reduce(
-          (total, item) => (total += item.quantity),
-          0
-        ),
-        averageScore:
-          item.Reviews.reduce((total, acc) => (total += acc.rating), 0) /
-          item.Reviews.length,
-        storeId: item.storeId,
-        storeName: item.storeName,
-        storeCity: item.Store.city,
-        storeImage: item.Store.image,
-      };
-    });
+    if (product.count === 0) {
+      return res.status(404).send({ message: "Product not found" });
+    }
+    const totalPage = product.length / dataPerPage;
+
+    if (currentPage > totalPage || currentPage < 1) {
+      return res.status(404).send({ message: "Page not found" });
+    }
+
+    const data =
+      product /
+      rows.map((item) => {
+        return {
+          id: item.id,
+          title: item.name,
+          category: item.Category.name,
+          description: item.description,
+          price: item.price,
+          stock: item.stock,
+          images: item.Images.map((item) => item.image),
+          sold: item?.OrderDetails.reduce(
+            (total, item) => (total += item.quantity),
+            0
+          ),
+          averageScore:
+            item.Reviews.reduce((total, acc) => (total += acc.rating), 0) /
+            item.Reviews.length,
+          storeId: item.storeId,
+          storeName: item.storeName,
+          storeCity: item.Store.city,
+          storeImage: item.Store.image,
+        };
+      });
     return res.status(200).send({
       success: true,
       message: "success",
       data: data,
+      totalProducts: product.count,
+      currentPage,
+      dataPerPage,
+      totalPage,
+      offset,
     });
   } catch (error) {
     return res.status(500).send(error.message);

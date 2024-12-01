aacb64f4602872ddb8d18367c8d61152070c189e
fiqrioemry <foemry@gmail.com>
2024-12-01 23:33:17 +0700

    fix issue

--- controllers/products/GetAllProducts.js
+++ controllers/products/GetAllProducts.js
@@ -1,3 +1,189 @@
+// const { Op } = require("sequelize");
+// const {
+//   Products,
+//   Stores,
+//   Images,
+//   Categories,
+//   Reviews,
+//   OrderDetails,
+//   Sequelize,
+// } = require("../../models");
+
+// module.exports = async (req, res) => {
+//   try {
+//     const {
+//       search,
+//       city,
+//       category,
+//       minPrice,
+//       maxPrice,
+//       minRating,
+//       maxRating,
+//       limit,
+//       page,
+//       sortBy,
+//       order,
+//     } = req.query;
+
+//     const dataPerPage = parseInt(limit) || 8;
+//     const currentPage = parseInt(page) || 1;
+//     const offset = (currentPage - 1) * dataPerPage;
+
+//     const query = {};
+//     if (search) {
+//       query[Op.or] = [
+//         { name: { [Op.like]: `%${search}%` } },
+//         { description: { [Op.like]: `${search}` } },
+//       ];
+//     }
+
+//     if (category) {
+//       const found = await Categories.findOne({
+//         where: { slug: category },
+//       });
+//       if (found) {
+//         query.categoryId = found.id;
+//       } else {
+//         return res.status(404).send({ message: "Category not found" });
+//       }
+//     }
+
+//     if (city) {
+//       const foundStores = await Stores.findAll({
+//         where: {
+//           city: Array.isArray(city) ? { [Op.in]: city } : city,
+//         },
+//       });
+//       if (foundStores.length > 0) {
+//         query.storeId = { [Op.in]: foundStores.map((store) => store.id) };
+//       } else {
+//         return res
+//           .status(404)
+//           .send({ message: "Stores not found in the specified cities" });
+//       }
+//     }
+
+//     if (minPrice && maxPrice) {
+//       query.price = {
+//         [Op.between]: [minPrice, maxPrice],
+//       };
+//     } else if (minPrice) {
+//       query.price = {
+//         [Op.gte]: minPrice,
+//       };
+//     } else if (maxPrice) {
+//       query.price = {
+//         [Op.lte]: maxPrice,
+//       };
+//     }
+
+//     const { rows, count } = await Products.findAndCountAll({
+//       offset: offset,
+//       limit: dataPerPage,
+//       where: query,
+//       include: [
+//         { model: Categories },
+//         {
+//           model: Stores,
+//         },
+//         {
+//           model: Images,
+//         },
+//         {
+//           model: OrderDetails,
+//         },
+//         {
+//           model: Reviews,
+//           attributes: [],
+//         },
+//       ],
+//       having: [
+//         ...(minRating
+//           ? [
+//               Sequelize.where(
+//                 Sequelize.fn("AVG", Sequelize.col("Reviews.rating")),
+//                 ">=",
+//                 minRating
+//               ),
+//             ]
+//           : []),
+//         ...(maxRating
+//           ? [
+//               Sequelize.where(
+//                 Sequelize.fn("AVG", Sequelize.col("Reviews.rating")),
+//                 "<=",
+//                 maxRating
+//               ),
+//             ]
+//           : []),
+//       ],
+
+//       attributes: {
+//         include: [
+//           [
+//             Sequelize.fn("AVG", Sequelize.col("Reviews.rating")),
+//             "averageRating",
+//           ],
+//           [Sequelize.fn("SUM", Sequelize.col("OrderDetails.quantity")), "sold"],
+//         ],
+//       },
+
+//       group: [
+//         "Products.id",
+//         "OrderDetails.id",
+//         "Images.id",
+//         "Reviews.id",
+//         "Category.id",
+//       ],
+//       distinct: true,
+//       order: [[sortBy || "createdAt", order || "asc"]],
+//     });
+
+//     if (count === 0) {
+//       return res.status(404).send({ message: "Product not found" });
+//     }
+//     const totalPage = Math.ceil(count / dataPerPage);
+
+//     if (currentPage > totalPage || currentPage < 1) {
+//       return res.status(404).send({ message: "Page not found" });
+//     }
+
+//     // const data = product.rows.map((item) => {
+//     //   return {
+//     //     id: item.id,
+//     //     title: item.name,
+//     //     slug: item.slug,
+//     //     category: item.Category.slug,
+//     //     description: item.description,
+//     //     price: item.price,
+//     //     stock: item.stock,
+//     //     images: item.Images[0].image,
+//     //     sold: item?.OrderDetails.reduce(
+//     //       (total, item) => (total += item.quantity),
+//     //       0
+//     //     ),
+//     //     storeId: item.storeId,
+//     //     storeName: item.Store.storeName,
+//     //     storeSlug: item.Store.slug,
+//     //     storeCity: item.Store.city,
+//     //     storeImage: item.Store.image,
+//     //   };
+//     // });
+//     return res.status(200).send({
+//       success: true,
+//       message: "success",
+//       data: rows,
+//       totalProducts: count,
+//       // currentPage,
+//       // dataPerPage,
+//       // totalPage,
+//       // offset,
+//     });
+//   } catch (error) {
+//     return res.status(500).send(error.message);
+//   }
+// };
+
 const { Op } = require("sequelize");
 const {
   Products,
@@ -23,7 +209,7 @@ module.exports = async (req, res) => {
       sortBy,
       order,
     } = req.query;
-
+    console.log(city);
     const dataPerPage = parseInt(limit) || 8;
     const currentPage = parseInt(page) || 1;
     const offset = (currentPage - 1) * dataPerPage;
@@ -32,7 +218,7 @@ module.exports = async (req, res) => {
     if (search) {
       query[Op.or] = [
         { name: { [Op.like]: `%${search}%` } },
-        { description: { [Op.like]: `%${search}%` } },
+        { description: { [Op.like]: `${search}` } },
       ];
     }
 
@@ -76,79 +262,70 @@ module.exports = async (req, res) => {
       };
     }
 
-    // Query ke database
-    const product = await Products.findAll({
+    const product = await Products.findAndCountAll({
       where: query,
+      limit: dataPerPage,
+      offset: offset,
       include: [
         {
           model: Reviews,
           attributes: ["rating"],
         },
         { model: Stores },
+
         { model: Images },
         { model: Categories },
         { model: OrderDetails },
       ],
+
+      distinct: true,
       order: [[sortBy || "createdAt", order || "asc"]],
     });
 
-    if (!product.length) {
+    if (product.count === 0) {
       return res.status(404).send({ message: "Product not found" });
     }
-
-    // Proses hasil query untuk filter minRating dan maxRating
-    const filteredData = product
-      .map((item) => {
-        const averageScore =
-          item.Reviews.length > 0
-            ? item.Reviews.reduce((acc, curr) => acc + curr.rating, 0) /
-              item.Reviews.length
-            : 0;
-
-        return {
-          id: item.id,
-          title: item.name,
-          slug: item.slug,
-          category: item.Category.slug,
-          description: item.description,
-          price: item.price,
-          stock: item.stock,
-          images: item.Images[0]?.image || null,
-          sold: item?.OrderDetails.reduce(
-            (total, detail) => (total += detail.quantity),
-            0
-          ),
-          averageScore,
-          storeId: item.storeId,
-          storeName: item.Store.storeName,
-          storeSlug: item.Store.slug,
-          storeCity: item.Store.city,
-          storeImage: item.Store.image,
-        };
-      })
-      .filter(
-        (item) =>
-          (!minRating || item.averageScore >= minRating) &&
-          (!maxRating || item.averageScore <= maxRating)
-      );
-
-    // Pagination setelah filter
-    const paginatedData = filteredData.slice(offset, offset + dataPerPage);
-    const totalProducts = filteredData.length;
-    const totalPage = Math.ceil(totalProducts / dataPerPage);
+    const totalPage = Math.ceil(product.count / dataPerPage);
 
     if (currentPage > totalPage || currentPage < 1) {
       return res.status(404).send({ message: "Page not found" });
     }
 
+    const data = product.rows.map((item) => {
+      return {
+        id: item.id,
+        title: item.name,
+        slug: item.slug,
+        category: item.Category.slug,
+        description: item.description,
+        price: item.price,
+        stock: item.stock,
+        images: item.Images[0].image,
+        sold: item?.OrderDetails.reduce(
+          (total, item) => (total += item.quantity),
+          0
+        ),
+        averageScore:
+          item.Reviews.reduce((acc, curr) => acc + curr.rating, 0) /
+          item.Reviews.length,
+
+        storeId: item.storeId,
+        storeName: item.Store.storeName,
+        storeSlug: item.Store.slug,
+        storeCity: item.Store.city,
+        storeImage: item.Store.image,
+      };
+    });
+
     return res.status(200).send({
       success: true,
       message: "success",
-      data: paginatedData,
-      totalProducts,
+      data: data,
+      totalProducts: product.count,
       currentPage,
       dataPerPage,
       totalPage,
+      offset,
     });
   } catch (error) {
     return res.status(500).send(error.message);
